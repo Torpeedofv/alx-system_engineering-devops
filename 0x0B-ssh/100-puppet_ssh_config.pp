19443ebb1ddaf592d7dda65d666a56bec715e891
Torpeedofv <torpeedo.fv@gmail.com>
2023-03-25 19:19:24 +0100

    commit

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -2,9 +2,13 @@
 
 file { 'config':
   ensure  => 'present',
-  content => 'Host myserver
+  content => 'Host *
+	PasswordAuthentication no
+
+Host myserver
 	User ubuntu
         PasswordAuthentication no
-	IdentityFile ~/.ssh/school',
+	IdentityFile ~/.ssh/school
+	Hostname 34.227.101.5',
   path    => '/home/torpeedo/.ssh/config',
 }
