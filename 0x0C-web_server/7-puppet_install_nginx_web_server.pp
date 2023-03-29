b9f5afaf7b3cbbe2eb2d9cd7376b8abf3cf037ad
Torpeedofv <torpeedo.fv@gmail.com>
2023-03-29 23:50:21 +0100

    commit

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -4,26 +4,14 @@ package { 'nginx':
   package_ensure => 'installed',
 }
 
-file { '/var/www/html/index.html':
-  ensure  => file,
+file { 'index.nginx-debian.html':
+  path    => '/var/www/html/index.nginx-debian.html',
   content => 'Hello World!',
-  mode    => '0644',
 }
 
-nginx::resource::server { 'default':
-  listen_port         => 80,
-  ipv6_listen_options => {
-    'listen'          => true,
-  },
-  locations           => {
-    '/redirect_me'    => {
-      returns => 301,
-      rewrite => ['^ /redirect_me(.*)$ https://www.youtube.com/watch?v=TfgBHC5gvTI$1 permanent'],
-    },
-  },
-  server_name         => '_',
-  root                => '/var/www/html',
-  index_files         => ['index.html', 'index.htm', 'index.nginx-debian.html'],
+exec { 'config':
+  command  => 'sed -i "s/server_name _;/server_name _;\n\trewrite ^\/redirect_me https:\/\/www.youtube.com/watch?v=TfgBHC5gvTI permanent;/" /etc/nginx/sites-available/default',
+  provider => 'shell',
 }
 exec { 'start':
   command  => 'sudo service nginx restart',
