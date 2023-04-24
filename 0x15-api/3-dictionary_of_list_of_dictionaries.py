fbb286fc1772df38810017420f9f887442ed7ec4
Torpeedofv <torpeedo.fv@gmail.com>
2023-04-24 19:32:49 +0100

    commit

---
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""records all tasks that are owned by an employee"""
+import json
+import requests
+import sys
+
+if __name__ == "__main__":
+    response_name = requests\
+        .get('https://jsonplaceholder.typicode.com/users/')
+    get_todo = requests\
+        .get('https://jsonplaceholder.typicode.com/users/{}/todos')
+    employee = response_name.json()
+    employee_todo = get_todo.json()
+    filename = 'todo_all_employees.json'
+
+    json_data = []
+    with open(filename, 'w') as file:
+        for employ in employee:
+            employee_id = employ['id']
+            employee_name = employ['username']
+            get_todo = requests\
+                .get('https://jsonplaceholder.typicode.com/users/{}/todos'
+                     .format(employee_id))
+            employee_todo = get_todo.json()
+            todo = []
+
+            for status in employee_todo:
+                task_status = status.get('completed')
+                task_title = status.get('title')
+                todo.append({"task": task_title,
+                            "completed": task_status,
+                             "username": employee_name})
+        json_data = {'{}'.format(employee_id): todo}
+        json.dump(json_data, file)
